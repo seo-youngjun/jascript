1adc5d54c33c3c2630191bf1170dd6f098f198ca
unknown <seoyoungjun>
2025-12-18 09:16:18 +0900

    *

---
+++ js/homework.js
@@ -0,0 +1,7 @@
+// 12월17일 숙제
+  let myName = '서영준';
+  console.log('이름은 ' + myName);
+  let myTel = '010-3770-1461';
+  console.log('전화번호는 ' + myTel);
+  let myAdress = '대구시 중구 중앙대로 403번지';
+  console.log('주소는 ' + myAdress);
