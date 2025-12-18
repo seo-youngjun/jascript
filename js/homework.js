e26457edf079f278709bee2142e4877355dd75b8
unknown <seoyoungjun>
2025-12-18 09:57:06 +0900

    branch연습

--- js/homework.js
+++ js/homework.js
@@ -1,7 +1,14 @@
 // 12월17일 숙제
-  let myName = '서영준';
-  console.log('이름은 ' + myName);
-  let myTel = '010-3770-1461';
-  console.log('전화번호는 ' + myTel);
-  let myAdress = '대구시 중구 중앙대로 403번지';
-  console.log('주소는 ' + myAdress);
+//  let myName = '서영준';
+//  console.log('이름은 ' + myName);
+//  let myTel = '010-3770-1461';
+//  console.log('전화번호는 ' + myTel);
+//  let myAdress = '대구시 중구 중앙대로 403번지';
+//  console.log('주소는 ' + myAdress);
+
+
+  let name = prompt("이름을 입력 : ");
+  let phone = prompt("연락처를 입력 : ");
+  let address = prompt("주소를 입력 : ");
+  console.log(`이름은 ${name}, 전화번호는 ${phone}, 주소는 ${address}`);
+  
--- todo_1217.js
+++ todo_1217.js
@@ -1,8 +1,15 @@
 // 15-2, 15-3장을 정독.
 // git 설치.
-// Dev/fun_javascript = git clone git 주소.
+
+// 원격(공유폴더)에서 전체폴더를 로컬(my pc)에 새로 생성시킬때
+// 프롬프트에서 Dev/fun_javascript 폴더에 가서 git clone git 깃허브주소(암호에 있는 주소 복사).
 // 이름, 연락처, 주소 정보를 입력하면
 // 콘솔에 "이름은 홍길동, 전화번호는 010-234-1234, 주소는 대구시 중구 중앙대로 403번지"
+
+// 로컬(my pc) -> 원격(공유폴더) 동기화
 // git add.
 // git commit -m "12/17 숙제완료"
 // git push -u origin main
+
+// 원격(공유폴더)-> 로컬(my pc) 동기화
+// git pull origin main
